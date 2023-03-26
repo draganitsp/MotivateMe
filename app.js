c75597e73804d8fd052a8dc67a9e77d3123c4b82
draganitsp <109218164+draganitsp@users.noreply.github.com>
2023-03-26 14:23:50 +0200

    Add files via upload
    MotivateMe app. It shows you how many weeks you have left, so you might want to start doing your big plans.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const body = document.querySelector('body');
-const inputYear = document.querySelector('.inputYear');
-const inputMonth = document.querySelector('.inputMonth');
-const inputDay = document.querySelector('.inputDay');
-const inputPlans = document.querySelector('.inputPlans');
+const inputYear = document.querySelector('.input-year');
+const inputMonth = document.querySelector('.input-month');
+const inputDay = document.querySelector('.input-day');
+const inputPlans = document.querySelector('.input-plans');
 
 let userYear;
 let userMonth;
@@ -15,14 +15,14 @@ const currentDay = today.getDay();
 let userPlans = '';
 
 const btnIndex = document.querySelector('.btn-index');
-const modalAge = document.querySelector('.modalAge');
+const modalAge = document.querySelector('.modal-age');
 btnIndex.addEventListener('click', function () {
   btnIndex.textContent = `LET'S DO THIS`;
-  btnIndex.classList.add('fadeOut');
+  btnIndex.classList.add('fade-out');
 
   setTimeout(function () {
     btnIndex.classList.add('hidden');
-    modalAge.classList.add('fadeIn');
+    modalAge.classList.add('fade-in');
     modalAge.classList.remove('hidden');
   }, 2700);
 
@@ -31,29 +31,29 @@ btnIndex.addEventListener('click', function () {
 });
 
 const btnApp = document.querySelector('.btn-app');
-const modalPlans = document.querySelector('.modalPlans');
+const modalPlans = document.querySelector('.modal-plans');
 btnApp.addEventListener('click', function () {
-  modalAge.classList.remove('fadeIn');
-  modalAge.classList.add('fadeOut');
+  modalAge.classList.remove('fade-in');
+  modalAge.classList.add('fade-out');
   btnApp.textContent = 'COOL';
   setTimeout(function () {
     modalAge.classList.add('hidden');
-    modalPlans.classList.add('fadeIn');
+    modalPlans.classList.add('fade-in');
     modalPlans.classList.remove('hidden');
   }, 2700);
 });
 let userAge;
 const btnResult = document.querySelector('.btn-result');
-const modalResult = document.querySelector('.modalResult');
+const modalResult = document.querySelector('.modal-result');
 btnResult.addEventListener('click', function () {
   userPlans = inputPlans.value;
-  modalPlans.classList.remove('fadeIn');
-  modalPlans.classList.add('fadeOut');
-  document.getElementById('yearText').classList.add('hidden');
+  modalPlans.classList.remove('fade-in');
+  modalPlans.classList.add('fade-out');
+  document.getElementById('year-text').classList.add('hidden');
   setTimeout(function () {
     modalPlans.classList.add('hidden');
   }, 2700);
-  modalResult.classList.add('fadeIn');
+  modalResult.classList.add('fade-in');
   modalResult.classList.remove('hidden');
   userYear = inputYear.value;
   userMonth = inputMonth.value;
@@ -62,7 +62,7 @@ btnResult.addEventListener('click', function () {
   if (userMonth - currentMonth > 0) userAge = userAge - 1;
   lifeWeeks = userAge * 12 * 4;
   if (userMonth - currentMonth > 0) lifeWeeks += userMonth - currentMonth * 4;
-  document.getElementById('yearText').textContent =
+  document.getElementById('year-text').textContent =
     "You're only  " +
     userAge +
     ' years old.' +
@@ -70,44 +70,44 @@ btnResult.addEventListener('click', function () {
     userPlans +
     ' , right?';
   setTimeout(function () {
-    document.getElementById('yearText').classList.add('fadeInText');
-    document.getElementById('yearText').classList.remove('hidden');
+    document.getElementById('year-text').classList.add('fade-in-text');
+    document.getElementById('year-text').classList.remove('hidden');
   }, 3300);
   setTimeout(function () {
-    document.getElementById('questionText').classList.add('fadeInText');
-    document.getElementById('questionText').classList.remove('hidden');
+    document.getElementById('question-text').classList.add('fade-in-text');
+    document.getElementById('question-text').classList.remove('hidden');
   }, 8300);
   setTimeout(function () {
-    document.querySelector('.btn-show').classList.add('fadeInText');
+    document.querySelector('.btn-show').classList.add('fade-in-text');
     document.querySelector('.btn-show').classList.remove('hidden');
   }, 12300);
 });
 
 const btnShow = document.querySelector('.btn-show');
 btnShow.addEventListener('click', function () {
-  modalResult.classList.remove('fadeIn');
-  modalResult.classList.add('fadeOut');
+  modalResult.classList.remove('fade-in');
+  modalResult.classList.add('fade-out');
   setTimeout(function () {
     modalResult.classList.add('hidden');
-    document.getElementById('month-container').classList.add('fadeIn');
+    document.getElementById('month-container').classList.add('fade-in');
     document.getElementById('month-container').classList.remove('hidden');
   }, 2000);
 
   for (let i = 0; i < 4160; i++) {
     setTimeout(function () {
       newdiv = document.createElement('div');
-      newdiv.classList.add('monthCubes');
+      newdiv.classList.add('month-cubes');
       document.getElementById('month-container').appendChild(newdiv);
       if (i < lifeWeeks) {
-        newdiv.classList.remove('monthCubes');
-        newdiv.classList.add('monthCubesSpent');
+        newdiv.classList.remove('month-cubes');
+        newdiv.classList.add('month-cubes-spent');
       } else {
-        document.getElementById('remaining').classList.add('fadeIn');
+        document.getElementById('remaining').classList.add('fade-in-text');
         document.getElementById('remaining').classList.remove('hidden');
         document.getElementById(
           'remaining'
         ).textContent = `${lifeWeeks} WEEKS SPENT.`;
-        document.getElementById('remaining2').classList.add('fadeInSlow');
+        document.getElementById('remaining2').classList.add('fade-in-slow');
         document.getElementById('remaining2').classList.remove('hidden');
         document.getElementById(
           'remaining2'
@@ -115,6 +115,6 @@ btnShow.addEventListener('click', function () {
           4100 - lifeWeeks
         } USEFUL. DO THE THING.`;
       }
-    }, i * 3);
+    }, i * 2);
   }
 });
